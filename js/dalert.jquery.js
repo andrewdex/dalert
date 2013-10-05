bbe872a9505bb35efe57b370ecd6a0b01bef08c9
Andrew dEX <andrewdexis@gmail.com>
2013-10-05 18:25:12 +0530

    //Added Protective Namespace for the functions

--- js/dalert.jquery.js
+++ js/dalert.jquery.js
@@ -1,15 +1,20 @@
 /*
 DAlert Simple JqueryPlugin
 Version: 1.0 (Beta)
-Dependencies : Jquery 1.6 + , Jquery UI 1.6 + Jquery UI CSS
+Author: Andrew dEX (Dilusha)
+Dependencies : Jquery 1.6 + , Jquery UI 1.6 + Jquery UI CSS Framework
 Info & API : http://www.dalert.andrewdex.com
 Contribute : http://www.github.com/andrewdex/dalert
 
 * dalert("Making the Alert Box much simpler, yet fancy");
 */
+
+//Default Dalert Implementation
+function dalert(msg,title) {
 	
-function dalert(title, msg) {
-	
+	if (title==null){
+		title="Dalert";
+	}
 	
 	if (msg !== null) {
 	
@@ -26,3 +31,38 @@ function dalert(title, msg) {
 	
 	
 }
+
+//
+var d = {
+		
+//		init: function()
+//		{
+//			
+//			dalert();
+//		},
+
+	    dalert: function(msg,title) {
+	    	
+	    	if (title==null){
+	    		title="Dalert";
+	    	}
+	    	
+	    	if (msg !== null) {
+	    	
+
+	    		$("body").append("<div id='dialog' style='display:none' title='" + title + "'>" + msg + "</div>");
+	    		$("#dialog").dialog({
+	    			modal:true
+	    			});
+	    	
+	    	} else {
+	    		return false;
+	    	
+	    	}
+	    	
+	    },
+
+	    confirm: function() {
+	    }
+	};
+//d.init();
