8a41b125f4052a9399a57826e24a5e712b84e357
Andrew dEX <andrewdexis@gmail.com>
2013-10-09 01:31:27 +0530

    Added Licensing

--- js/dalert.jquery.js
+++ js/dalert.jquery.js
@@ -1,13 +1,35 @@
 /*
-DAlert jQueryPlugin
-Version: 1.0 (Beta)
-Author: Andrew dEX (Dilusha)
-Dependencies : Jquery 1.6 + , Jquery UI 1.6 + Jquery UI CSS Framework
-Info & API : http://dalert.andrewdex.com
-Contribute : http://www.github.com/andrewdex/dalert
+* DAlert jQueryPlugin
+* dalert.alert("Making the Alert Box much simpler, yet fancy");
+* Version: 1.0 (Beta)
+* Author: Andrew dEX (Dilusha)
+* Dependencies : Jquery 1.6 + , Jquery UI 1.6 + Jquery UI CSS Framework
+* Info & API : http://dalert.andrewdex.com
+* Contribute : http://www.github.com/andrewdex/dalert
+* 
+* Copyright 2013 Andrew dEX (Dilusha Gonagala)
+* 
+* Released under the MIT license
+*http://opensource.org/licenses/MIT
+*Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
- * dalert("Making the Alert Box much simpler, yet fancy");
- */
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*
+*/
 
 var dalert = {
 
@@ -101,7 +123,48 @@ var dalert = {
 		}
 
 	},
+	
+	prompt : function(prompt_msg) {
+		var dprompt_v = "dex is now";
+		if (!prompt_msg)
+			prompt_msg = 'What is your name please? ';
 
+		//		    if (!FontColor)
+		//		    	FontColor = 'black';
+		//		    
+		//		    if(!bodyColor)
+		//		    	bodyColor='rgb(10, 60, 65)';
+
+		//    try {
+		$("<div></div>").html(
+				prompt_msg + "<input name='dprompt_val' type='text'/>").dialog(
+				{
+					title : 'hellow',
+					resizable : false,
+					modal : true,
+					buttons : {
+						"Ok" : function() {
+							alert("ssss");
+							dprompt_v = $(this).find(
+									'input[name="dprompt_val"]').val();
+							alert(dprompt_v);
+							$(this).dialog("close");
+
+						}
+					}
+				});
+
+		//		        $(".ui-widget-content").css("color", FontColor);
+		//		        $(".ui-widget-content").css("background", bodyColor);
+
+		//    }
+		//    catch (error) {
+
+		//        alert(prompt_msg);
+		//    }
+
+		return dprompt_v;
+	},
 	//Replace DAlert With Window JavaScript Original objects : alert, confirm etc.	
 	ReplaceAlert : function() {
 
