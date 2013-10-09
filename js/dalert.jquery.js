48c97991bccaf93fc78518ef2ce613c6f5e47b53
Andrew dEX <andrewdexis@gmail.com>
2013-10-09 13:01:41 +0530

    License Modified

--- js/dalert.jquery.js
+++ js/dalert.jquery.js
@@ -9,9 +9,10 @@
  * 
  * Copyright 2013 Andrew dEX (Dilusha Gonagala)
  * 
- * Released under the MIT license
- *http://opensource.org/licenses/MIT
- *Permission is hereby granted, free of charge, to any person obtaining a copy
+Released under the MIT license
+_______________________________
+http://opensource.org/licenses/MIT
+Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
