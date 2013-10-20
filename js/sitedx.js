cfb5fddff13af1e3a222d83a17bf12dc53903278
Andrew dEX <andrewdexis@gmail.com>
2013-10-21 00:54:40 +0530

    Completed Basic Usage of API

--- js/sitedx.js
+++ js/sitedx.js
@@ -4,9 +4,10 @@
  * */
 
 
-$("#simple_dalert").click(function() {
-	$("#dalert_home").removeClass("active");
-	$("#replace_alert").removeClass("active");
+$("#simple_dalert_link").click(function() {
+	$("#dalert_home_link").removeClass("active");
+	$("#replace_alert_link").removeClass("active");
+	$("#replace_confirm_link").removeClass("active");
 	$(this).addClass("active");
     $('html,body').animate({
         scrollTop: $("#dalert_simple").offset().top
@@ -14,20 +15,34 @@ $("#simple_dalert").click(function() {
 });
 
 
-$("#dalert_home").click(function() {
-	$("#simple_dalert").removeClass("active");
-	$("#replace_alert").removeClass("active");
+$("#dalert_home_link").click(function() {
+	$("#simple_dalert_link").removeClass("active");
+	$("#replace_alert_link").removeClass("active");
+	$("#replace_confirm_link").removeClass("active");
 	$(this).addClass("active");
 
 });
 
 
 
-$("#replace_alert").click(function() {
-	$("#dalert_home").removeClass("active");
-	$("#simple_dalert").removeClass("active");
+$("#replace_alert_link").click(function() {
+	$("#dalert_home_link").removeClass("active");
+	$("#simple_dalert_link").removeClass("active");
+	$("#replace_confirm_link").removeClass("active");
 	$(this).addClass("active");
     $('html,body').animate({
         scrollTop: $("#alert_replace").offset().top
     }, 1000);
 });
+
+$("#replace_confirm_link").click(function() {
+	$("#dalert_home_link").removeClass("active");
+	$("#simple_dalert_link").removeClass("active");
+	$("#replace_alert_link").removeClass("active");
+	$(this).addClass("active");
+    $('html,body').animate({
+        scrollTop: $("#confirm_replace").offset().top
+    }, 1000);
+});
+
+
