80d063d7aa0e6d2c73581ce5afade2439596629a
Andrew dEX <andrewdexis@gmail.com>
2013-10-20 23:55:06 +0530

    Updates

--- js/sitedx.js
+++ js/sitedx.js
@@ -5,8 +5,16 @@
 
 
 $("#simple_dalert").click(function() {
-	
+	$("#dalert_home").removeClass("active");
+	$(this).addClass("active");
     $('html,body').animate({
         scrollTop: $("#dalert_simple").offset().top
     }, 1000);
+});
+
+
+$("#dalert_home").click(function() {
+	$("#simple_dalert").removeClass("active");
+	$(this).addClass("active");
+
 });
