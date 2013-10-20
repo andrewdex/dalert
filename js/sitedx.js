d501e39f340e5c05cf0a231c840b096e2ef9179f
Andrew dEX <andrewdexis@gmail.com>
2013-10-21 00:44:42 +0530

    Latest Changes

--- js/sitedx.js
+++ js/sitedx.js
@@ -6,6 +6,7 @@
 
 $("#simple_dalert").click(function() {
 	$("#dalert_home").removeClass("active");
+	$("#replace_alert").removeClass("active");
 	$(this).addClass("active");
     $('html,body').animate({
         scrollTop: $("#dalert_simple").offset().top
@@ -15,6 +16,18 @@ $("#simple_dalert").click(function() {
 
 $("#dalert_home").click(function() {
 	$("#simple_dalert").removeClass("active");
+	$("#replace_alert").removeClass("active");
 	$(this).addClass("active");
 
+});
+
+
+
+$("#replace_alert").click(function() {
+	$("#dalert_home").removeClass("active");
+	$("#simple_dalert").removeClass("active");
+	$(this).addClass("active");
+    $('html,body').animate({
+        scrollTop: $("#alert_replace").offset().top
+    }, 1000);
 });
