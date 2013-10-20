d5a92a31109de64e042bf6839b5880000d138b27
Andrew dEX <andrewdexis@gmail.com>
2013-10-20 23:36:40 +0530

    Added Scripting And Removed Samples

--- js/sitedx.js
+++ js/sitedx.js
@@ -4,9 +4,9 @@
  * */
 
 
-$("#dalert").click(function() {
-	dalert.alert("hey");
-    $('html').animate({
-        scrollTop: $("html").offset().top
+$("#simple_dalert").click(function() {
+	
+    $('html,body').animate({
+        scrollTop: $("#dalert_simple").offset().top
     }, 1000);
 });
