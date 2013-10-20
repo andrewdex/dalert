d7273b105c9c5c23054bd659d9d96dcf7a04b924
Andrew dEX <andrewdexis@gmail.com>
2013-10-21 01:40:45 +0530

    Parameters

--- js/sitedx.js
+++ js/sitedx.js
@@ -8,6 +8,7 @@ $("#simple_dalert_link").click(function() {
 	$("#dalert_home_link").removeClass("active");
 	$("#replace_alert_link").removeClass("active");
 	$("#replace_confirm_link").removeClass("active");
+	$("#parameterized_link").removeClass("active");
 	$(this).addClass("active");
     $('html,body').animate({
         scrollTop: $("#dalert_simple").offset().top
@@ -19,6 +20,7 @@ $("#dalert_home_link").click(function() {
 	$("#simple_dalert_link").removeClass("active");
 	$("#replace_alert_link").removeClass("active");
 	$("#replace_confirm_link").removeClass("active");
+	$("#parameterized_link").removeClass("active");
 	$(this).addClass("active");
 
 });
@@ -29,6 +31,7 @@ $("#replace_alert_link").click(function() {
 	$("#dalert_home_link").removeClass("active");
 	$("#simple_dalert_link").removeClass("active");
 	$("#replace_confirm_link").removeClass("active");
+	$("#parameterized_link").removeClass("active");
 	$(this).addClass("active");
     $('html,body').animate({
         scrollTop: $("#alert_replace").offset().top
@@ -39,9 +42,23 @@ $("#replace_confirm_link").click(function() {
 	$("#dalert_home_link").removeClass("active");
 	$("#simple_dalert_link").removeClass("active");
 	$("#replace_alert_link").removeClass("active");
+	$("#parameterized_link").removeClass("active");
 	$(this).addClass("active");
     $('html,body').animate({
         scrollTop: $("#confirm_replace").offset().top
+    }, 1000);
+});
+
+
+
+$("#parameterized_link").click(function() {
+	$("#dalert_home_link").removeClass("active");
+	$("#simple_dalert_link").removeClass("active");
+	$("#replace_alert_link").removeClass("active");
+	$("#replace_confirm_link").removeClass("active");
+	$(this).addClass("active");
+    $('html,body').animate({
+        scrollTop: $("#dalert_paramters").offset().top
     }, 1000);
 });
 
